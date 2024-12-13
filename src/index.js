4d73bcb42c72d117359da3be954c619237d3f308
Ace De Castro <acetdecastro@gmail.com>
2024-12-13 14:16:38 +0800

    Add cart simulation for bulk discount

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import { PRODUCTS, PROMOS } from "./constants/index.js";
 const pricingRules = new PricingRules();
 const cart = new ShoppingCart(pricingRules);
 
+// 3 for 2
+// cart.add(PRODUCTS.ULT_SMALL.code);
+// cart.add(PRODUCTS.ULT_SMALL.code);
+// cart.add(PRODUCTS.ULT_SMALL.code);
+// cart.add(PRODUCTS.ULT_LARGE.code);
+
+// 5GB bulk discount
 cart.add(PRODUCTS.ULT_SMALL.code);
 cart.add(PRODUCTS.ULT_SMALL.code);
-cart.add(PRODUCTS.ULT_SMALL.code);
+cart.add(PRODUCTS.ULT_LARGE.code);
+cart.add(PRODUCTS.ULT_LARGE.code);
+cart.add(PRODUCTS.ULT_LARGE.code);
 cart.add(PRODUCTS.ULT_LARGE.code);
 
 console.log("Cart Total:", cart.total());
